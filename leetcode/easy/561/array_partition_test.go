c0c0fa76062b2c3d6f2052b4a0529fa83fa625a5
Ожегов Андрей Геннадьевич <ozhegov@bpm-cons.ru>
2024-02-17 14:15:10 +0300

    561 done

---
+++ leetcode/easy/561/array_partition_test.go
@@ -0,0 +1,45 @@
+package _561_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"slices"
+	"testing"
+)
+
+func TestArrayPartition(t *testing.T) {
+	cases := []struct {
+		name string
+		nums []int
+		res  int
+	}{
+		{
+			name: "1",
+			nums: []int{1, 4, 3, 2},
+			res:  4,
+		},
+		{
+			name: "2",
+			nums: []int{6, 2, 6, 5, 1, 2},
+			res:  9,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			res := arrayPairSum(c.nums)
+			assert.Equal(t, c.res, res)
+		})
+	}
+}
+
+func arrayPairSum(nums []int) int {
+	slices.Sort(nums)
+	var sum int
+	for i := 0; i < len(nums); i += 2 {
+		sum += nums[i]
+	}
+
+	return sum
+}
