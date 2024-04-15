3e88baa1dae1e9d770a42283c8ac2288ea5d7db7
Ожегов Андрей Геннадьевич <ozhegov@bpm-cons.ru>
2024-04-15 21:01:00 +0300

    705 done

---
+++ leetcode/easy/705/hash_set_test.go
@@ -0,0 +1,38 @@
+package _705_test
+
+import "testing"
+
+func TestHashSet(t *testing.T) {
+
+}
+
+type MyHashSet struct {
+	store map[int]struct{}
+}
+
+func Constructor() MyHashSet {
+	return MyHashSet{
+		store: make(map[int]struct{}),
+	}
+}
+
+func (this *MyHashSet) Add(key int) {
+	this.store[key] = struct{}{}
+}
+
+func (this *MyHashSet) Remove(key int) {
+	delete(this.store, key)
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+	_, ok := this.store[key]
+	return ok
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
