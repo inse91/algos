c1018c147bdf764973cf840e59f5c5f62d09c1ac
Ожегов Андрей Геннадьевич <ozhegov@bpm-cons.ru>
2025-08-11 15:02:59 +0300

    2651

---
+++ leetcode/easy/2651/find_delayed_time_test.go
@@ -0,0 +1,40 @@
+package _2651_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// https://leetcode.com/problems/calculate-delayed-arrival-time
+
+func Test(t *testing.T) {
+	cases := []struct {
+		name string
+		time int
+		delay int
+		res   int
+	}{
+		{
+			name: "1",
+			time: 15,
+			delay: 5,
+			res: 20,
+		},
+		{
+			name: "2",
+			time: 13,
+			delay: 11,
+			res: 0,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			assert.Equal(t, c.res, findDelayedArrivalTime(c.time, c.delay))
+		})
+	}
+}
+
+func findDelayedArrivalTime(arrivalTime int, delayedTime int) int {
+	return (arrivalTime + delayedTime) % 24
+}
